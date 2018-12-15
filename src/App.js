7eb741f5c266fcdd2284ed5777637ad91a16bb62
Vova Nagai <ghostfalcon.kr@gmail.com>
2018-12-15 16:22:01 +0200

    add heder+nav+main

--- src/App.js
+++ src/App.js
@@ -3,31 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Header from './components/header';
+import Menu from './components/menu';
+import Main from './components/main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`;
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+`;
+
+const MainWrapper = styled.main`
+    height: calc(100vh - 49px - 89px);
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`;
+
 
 class App extends Component {
   render() {
     return (
-        <div className='wrapper'>
-            <SayFullName name='Vova' surname='Nagai' link='https://github.com/VovaNagai' />
-            <SayFullName name='lorem1' surname='LOLO' link='https://github.com/VovaNagai' />
-            <SayFullName name='lorem2' surname='OLO' link='#' />
+        <div className='App'>
+            <HeaderWrapper>
+                <Grid>
+                    <Header/>
+                </Grid>
+            </HeaderWrapper>
+            <MenuWrapper>
+                <Grid>
+                    <Menu/>
+                </Grid>
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main/>
+                </Grid>
+            </MainWrapper>
         </div>
     );
   }
 }
 
-
-
-function SayFullName(props) {
-    return (
-        <div>
-            <h1>Мое имя {props.name}, фамилия - {props.surname}</h1>
-            <a href={props.link}>Cсылка на мой профиль</a>
-        </div>
-    )
-}
 
 export default App;
